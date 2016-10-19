d76d47c50455e4586ca0e9b22250434a00183e75
andreybelitsky <andrey.belitsky@gmail.com>
2016-10-19 14:58:11 -0400

    Please enter thessue with the spring boot runner. Implemented the  value-objects for JSON mapping, implemented CSV file writing  routine(still need to work on the format of the resulting CSV).

---
+++ src/main/java/GoEuroTest/vo/GeoPositionVO.java
@@ -0,0 +1,53 @@
+package GoEuroTest.vo;
+
+/**
+ * Created by Andrii on 10/19/2016.
+ */
+public class GeoPositionVO {
+    private String latitude;
+    private String longitude;
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GeoPositionVO that = (GeoPositionVO) o;
+
+        if (latitude != null ? !latitude.equals(that.latitude) : that.latitude != null) return false;
+        if (longitude != null ? !longitude.equals(that.longitude) : that.longitude != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = latitude != null ? latitude.hashCode() : 0;
+        result = 31 * result + (longitude != null ? longitude.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "GeoPositionVO{" +
+                "latitude='" + latitude + '\'' +
+                ", longitude='" + longitude + '\'' +
+                '}';
+    }
+}
