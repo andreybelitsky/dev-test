5aafaae85cd132d575d1415ec8fb062185bedcef
Andrii Bilytskyi <andrey.belitsky@gmail.com>
2016-10-19 19:28:54 +0300

    Added the maven plugin that allows java 8 compiling.

--- src/main/java/GoEuroTest/service/impl/CityRestCSVLoadingService.java
+++ src/main/java/GoEuroTest/service/impl/CityRestCSVLoadingService.java
@@ -1,19 +1,23 @@
 package GoEuroTest.service.impl;
 
+import GoEuroTest.service.CSVLoadingService;
+import GoEuroTest.vo.CityVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import GoEuroTest.service.CSVLoadingService;
-import GoEuroTest.vo.CityVO;
 
 import java.util.List;
 
 @Service
 public class CityRestCSVLoadingService implements CSVLoadingService {
     public static final String CITY_REST_API = "http://api.goeuro.com/api/v2/position/suggest/en/%s";
+    private static final Logger log = LoggerFactory.getLogger(CityRestCSVLoadingService.class);
+
     @Autowired
     RestTemplateBuilder builder;
 
